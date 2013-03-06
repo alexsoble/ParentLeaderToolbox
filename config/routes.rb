5fc6a7e71ccffeec9fc977b9384fe05130abcf83
alexsoble <asoble@gmail.com>
2013-03-06 06:21:47 -0600

    Combined HTML/CSS mockups with Rails framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 ParentToolbox::Application.routes.draw do
 	root to: 'tools#index'
 	
+  get '/about' => 'tools#about'
+	get '/home' => 'tools#index'
+	get '/tools' => 'tools#tools'
+	get '/signin' => 'tools#signin'
+	
 	resources :users
-	resources :tools
+  resources :tools
 	resources :categories
 	
 end
