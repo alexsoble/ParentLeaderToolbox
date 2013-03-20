48416b281d726ccf72af4d1c93ebe0b71cb54830
alexsoble <asoble@gmail.com>
2013-03-20 07:03:24 -0500

    Messing around with user images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319203535) do
+ActiveRecord::Schema.define(:version => 20130320113059) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(:version => 20130319203535) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "image"
   end
 
 end
